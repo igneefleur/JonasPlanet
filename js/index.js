94567455f586f82f732e0f1f28076af4752178d7
Me-k_01 <mecaman01@gmail.com>
2021-02-10 18:15:21 +0100

    html

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// const turn = document.getElementById('turn');
+// const bg = document.getElementById('bg');
+// bg.style.width = window.innerWidth;
+// bg.style.height = window.innerHeight;
+// let angle = 0;
+
+//
+// setInterval(50, () => {
+//
+//   turn.style.
+// })
